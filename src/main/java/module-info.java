2ab8986a74be79abb7619a8d501906c68eee1eb8
LynAguayo <lyn.aguayoc@gmail.com>
2025-05-16 12:29:59 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.ecotracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.ecotracker to javafx.fxml;
+    exports com.example.ecotracker;
+}
