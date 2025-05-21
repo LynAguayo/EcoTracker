6e0cc971eefbca12a425955ef29d2ebb4cb40ad2
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-05-21 18:45:53 +0200

    declarar variables activitat

--- src/main/java/com/example/ecotracker/model/Activitat.java
+++ src/main/java/com/example/ecotracker/model/Activitat.java
@@ -1,4 +1,16 @@
 package com.example.ecotracker.model;
 
+import java.time.LocalDate;
+
+/**
+ * Aquesta classe representa una activitat sostenible.
+ * Cada activitat té un nom, una data, una categoria, una descripció i un valor estimat de CO2 estalviat.
+ */
 public class Activitat {
+    private String nom;
+    private LocalDate data;
+    private String categoria;
+    private String descripcio;
+    private double valorCO2;
+
 }
