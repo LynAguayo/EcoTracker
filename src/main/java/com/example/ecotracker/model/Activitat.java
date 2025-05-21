78d31562c3fe90bab7c43754280e994320c191e0
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-05-21 18:53:07 +0200

    crear constructor activitat

--- src/main/java/com/example/ecotracker/model/Activitat.java
+++ src/main/java/com/example/ecotracker/model/Activitat.java
@@ -7,10 +7,21 @@ import java.time.LocalDate;
  * Cada activitat té un nom, una data, una categoria, una descripció i un valor estimat de CO2 estalviat.
  */
 public class Activitat {
-    private String nom;
-    private LocalDate data;
-    private String categoria;
-    private String descripcio;
-    private double valorCO2;
+    private String nom; // nom de l'activitat
+    private LocalDate data; // data en què s'ha realitzat l'activitat
+    private String categoria; // categoria de l'activitat
+    private String descripcio; // descripció de l'activitat
+    private double valorCO2; // quantitat estimada de C=2 estalviat durant l'activitat
+
+    /**
+     * Constructor per inicialitzar una activitat amb tots els seus camps.
+     */
+    public Activitat(String nom, LocalDate data, String categoria, String descripcio, double valorCO2) {
+        this.nom = nom;
+        this.data = data;
+        this.categoria = categoria;
+        this.descripcio = descripcio;
+        this.valorCO2 = valorCO2;
+    }
 
 }
