5e2fef07ebaf4b06f2088833f5752fd14d765ebb
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-05-22 11:45:11 +0200

    Importar llibreries

--- src/main/java/com/example/ecotracker/EcoTrackerApp.java
+++ src/main/java/com/example/ecotracker/EcoTrackerApp.java
@@ -1,7 +1,14 @@
 package com.example.ecotracker;
 
+import com.example.ecotracker.controller.MenuInicialController;
+import com.example.ecotracker.dao.ActivitatDAO;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class EcoTrackerApp extends Application {
 
