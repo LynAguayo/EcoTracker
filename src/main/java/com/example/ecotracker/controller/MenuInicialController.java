26c37764cf05571a46492cf9cb44e018285e5e05
LynAguayo <lyn.aguayoc@gmail.com>
2025-05-22 10:21:40 +0200

    Importar libs per MenuInicialController

---
+++ src/main/java/com/example/ecotracker/controller/MenuInicialController.java
@@ -0,0 +1,13 @@
+package com.example.ecotracker.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuInicialController {
+}
