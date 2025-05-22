309308aaa518dd8f83f653c64814841738090d3c
LynAguayo <lyn.aguayoc@gmail.com>
2025-05-22 11:09:34 +0200

    Importar libs per visualitzar taules controller

---
+++ src/main/java/com/example/ecotracker/controller/VisualitzarActivitatsController.java
@@ -0,0 +1,27 @@
+package com.example.ecotracker.controller;
+
+import com.example.ecotracker.dao.ActivitatDAO;
+import com.example.ecotracker.model.Activitat;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class VisualitzarActivitatsController {
+}
