35ce3f7ba020eb9d4b329e67bc3550769e935780
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2025-05-22 12:37:26 +0200

    Actualizar module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.ecotracker {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.sql;
+    requires mysql.connector.java;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -10,6 +12,13 @@ module com.example.ecotracker {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
 
+    // Obrir paquets per FXML
     opens com.example.ecotracker to javafx.fxml;
+    opens com.example.ecotracker.controller to javafx.fxml; // importante
+    opens com.example.ecotracker.model to javafx.base;
+
+    // Exporta els paquets que necessiten ser accedits des d'altres mòduls
     exports com.example.ecotracker;
+    exports com.example.ecotracker.model;
+    exports com.example.ecotracker.controller;
 }
