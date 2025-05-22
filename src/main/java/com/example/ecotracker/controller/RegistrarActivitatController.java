cd914ffd8564e2964642700986d8d1c51b14f9af
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-05-22 10:11:51 +0200

    Importar llibreries

--- src/main/java/com/example/ecotracker/controller/RegistrarActivitatController.java
+++ src/main/java/com/example/ecotracker/controller/RegistrarActivitatController.java
@@ -1,4 +1,18 @@
 package com.example.ecotracker.controller;
 
+import com.example.ecotracker.dao.ActivitatDAO;
+import com.example.ecotracker.model.Activitat;
+import com.example.ecotracker.util.CO2Calculator;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
 public class RegistrarActivitatController {
+    
 }
