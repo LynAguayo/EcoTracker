366970392fa66dac3186d6474c8bbddb19e88d37
LynAguayo <lyn.aguayoc@gmail.com>
2025-05-22 18:51:54 +0200

    Test de Inserts a la bbdd amb Mockito

--- src/test/java/dao/ActivitatDAOTest.java
+++ src/test/java/dao/ActivitatDAOTest.java
@@ -38,3 +38,39 @@ class ActivitatDAOTest {
         when(mockConnection.prepareStatement(anyString(), anyInt())).thenReturn(mockPreparedStatement);
         when(mockConnection.createStatement()).thenReturn(mockStatement);
     }
+
+    @Test
+    void testInsert() throws SQLException {
+        try (MockedStatic<DBConnector> mocked = Mockito.mockStatic(DBConnector.class)) {
+            // Simulem la connexió a la base de dades
+            mocked.when(DBConnector::getConnection).thenReturn(mockConnection);
+
+            // Simulem l'execució de la sentència d'inserció
+            when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+            when(mockPreparedStatement.getGeneratedKeys()).thenReturn(mockResultSet);
+            when(mockResultSet.next()).thenReturn(true);
+            when(mockResultSet.getInt(1)).thenReturn(1);
+
+            // Creem una activitat d'exemple
+            Activitat activitat = new Activitat(
+                    "Bicicleta a la feina",
+                    LocalDate.of(2025, 1, 10),
+                    "Transport",
+                    "Vaig anar en bicicleta 10 km a la feina",
+                    2.4
+            );
+
+            // Inserim l'activitat
+            activitatDAO.insert(activitat);
+
+            // Comprovem que l'ID s'ha assignat correctament
+            assertEquals(Integer.valueOf(1), activitat.getId());
+
+            // Verifiquem que els paràmetres s'han assignat correctament a la sentència
+            verify(mockPreparedStatement).setString(1, "Bicicleta a la feina");
+            verify(mockPreparedStatement).setDate(2, Date.valueOf(LocalDate.of(2025, 1, 10)));
+            verify(mockPreparedStatement).setString(3, "Transport");
+            verify(mockPreparedStatement).setString(4, "Vaig anar en bicicleta 10 km a la feina");
+            verify(mockPreparedStatement).setDouble(5, 2.4);
+        }
+    }
