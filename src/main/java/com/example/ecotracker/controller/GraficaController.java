95397f8e2625cb2966d2fd98e8165200f4747d5d
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2025-05-22 12:19:25 +0200

    Añadir comentarios y funcion para volver atrás

--- src/main/java/com/example/ecotracker/controller/GraficaController.java
+++ src/main/java/com/example/ecotracker/controller/GraficaController.java
@@ -2,6 +2,9 @@ package com.example.ecotracker.controller;
 
 import com.example.ecotracker.dao.ActivitatDAO;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -30,11 +33,35 @@ public class GraficaController {
         this.primaryStage = primaryStage;
     }
 
+    /**
+     * Inicialitza el controlador després de carregar-se la vista.
+     * Carrega les dades del gràfic.
+     */
     @FXML
     public void initialize() {
         updateChart();
     }
 
+    /**
+     * Gestor de l'acció del botó per tornar al menú inicial.
+     * Canvia l'escena a la vista principal del menú.
+     */
+    @FXML
+    private void handleBack() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ecotracker/menu-inicial.fxml"));
+            Parent root = loader.load();
+            MenuInicialController controller = loader.getController();
+            controller.setPrimaryStage(primaryStage);
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("EcoTracker - Menú Principal");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     private void updateChart() {}
+
 
 }
